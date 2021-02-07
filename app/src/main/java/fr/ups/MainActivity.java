edf76e99ca3c6d84149c50a048f4057b5f55ac91
juliabiesiada <jbiesiada@outlook.com>
2021-02-07 19:36:29 +0100

    Backup

--- app/src/main/java/fr/ups/MainActivity.java
+++ app/src/main/java/fr/ups/MainActivity.java
@@ -2,8 +2,13 @@ package fr.ups;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 import com.wikitude.NativeStartupConfiguration;
 import com.wikitude.WikitudeSDK;
 import com.wikitude.common.WikitudeError;
@@ -14,6 +19,9 @@ import com.wikitude.tracker.ImageTarget;
 import com.wikitude.tracker.ImageTracker;
 import com.wikitude.tracker.ImageTrackerListener;
 import com.wikitude.tracker.TargetCollectionResource;
+
+import java.util.Arrays;
+
 import fr.ups.wikitude.rendering.CustomSurfaceView;
 import fr.ups.wikitude.rendering.Driver;
 import fr.ups.wikitude.rendering.GLRenderer;
@@ -27,10 +35,18 @@ public class MainActivity extends Activity implements ImageTrackerListener, Exte
     private CustomSurfaceView customSurfaceView;
     private Driver driver;
     private GLRenderer glRenderer;
+    Handler handler = new Handler(Looper.getMainLooper());
+    //to post data
+    String[] field = new String[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        field[0] = "tid";
+        field[1] = "type";
+        field[2] = "text";
+        field[3] = "image";
 
         wikitudeSDK = new WikitudeSDK(this);
         NativeStartupConfiguration startupConfiguration = new NativeStartupConfiguration();
@@ -91,6 +107,7 @@ public class MainActivity extends Activity implements ImageTrackerListener, Exte
         Log.v(TAG, "Recognized target " + target.getName());
         StrokedRectangle strokedRectangle = new StrokedRectangle(StrokedRectangle.Type.STANDARD);
         glRenderer.setRenderablesForKey(target.getName() + target.getUniqueId(), strokedRectangle, null);
+        sendData();
     }
 
     @Override
@@ -114,5 +131,34 @@ public class MainActivity extends Activity implements ImageTrackerListener, Exte
     @Override
     public void onExtendedTrackingQualityChanged(ImageTracker imageTracker, ImageTarget imageTarget, int i, int i1) {
 
+    }
+
+    public void sendData() {
+        //Start ProgressBar first (Set visibility VISIBLE)
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                //Creating array for data
+                String[] data = new String[4];
+                data[0] = "1";
+                data[1] = "text";
+                data[2] = "Hello!";
+                data[3] = null;
+                PutData putData = new PutData("https://192.168.1.87/LoginRegister/addNote.php", "POST", field, data);
+                if (putData.startPut()) {
+                    if (putData.onComplete()) {
+                        String result = putData.getResult();
+                        if (result == "Adding Successful") {
+                            Toast.makeText(MainActivity.this,
+                                    "adding successful",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                        //End ProgressBar (Set visibility to GONE)
+                    }
+                }
+                //End Write and Read data with URL
+            }
+        });
     }
 }
